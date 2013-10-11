fb8fc6da2a60376af12aecd716b670edf6a3718e
Régis Hanol <regis@hanol.fr>
2013-10-11 18:37:05 +0200

    run the spoiler alert plugin on the next runloop

--- assets/javascripts/spoiler_alert_main.js
+++ assets/javascripts/spoiler_alert_main.js
@@ -6,11 +6,14 @@
       $('.spoiler:has(img)', $post).removeClass('spoiler').spoilerAlert({max: 20, partial: 6}).wrap("<div style='display: inline-block; overflow: hidden;'></div>");
       $('.spoiler:not(:has(img))', $post).removeClass('spoiler').spoilerAlert({max: 4, partial: 2});
     };
+    
+    Em.run.next(function() {
+      applySpoilers();
+      
+      Discourse.PostView.prototype.on("postViewInserted", applySpoilers);
+      Discourse.ComposerView.prototype.on("previewRefreshed", applySpoilers);
+    });
 
-    Discourse.PostView.prototype.on("postViewInserted", applySpoilers);
-    Discourse.ComposerView.prototype.on("previewRefreshed", applySpoilers);
-
-    applySpoilers();
   });
 
 })();
