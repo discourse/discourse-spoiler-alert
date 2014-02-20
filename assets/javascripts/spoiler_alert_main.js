153f96730c256d5f1fa1fa2d7edb1246f4276f6b
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-20 15:18:18 +0530

    Apply spoilers to the entire user activity stream.

--- assets/javascripts/spoiler_alert_main.js
+++ assets/javascripts/spoiler_alert_main.js
@@ -17,6 +17,7 @@
 
       Discourse.PostView.prototype.on("postViewInserted", applySpoilers);
       Discourse.ComposerView.prototype.on("previewRefreshed", applySpoilers);
+      Discourse.UserStreamView.prototype.on("didInsertElement", applySpoilers);
     });
 
   });
