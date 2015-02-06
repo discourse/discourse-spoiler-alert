8b236ae815145a9cbd2016d81c5078d269bc4471
Robin Ward <robin.ward@gmail.com>
2015-02-06 18:22:41 -0500

    Add `url` metadata

--- plugin.rb
+++ plugin.rb
@@ -2,5 +2,6 @@
 # about: Uses the Spoiler Alert plugin to blur text when spoiling it.
 # version: 0.1
 # authors: Robin Ward, Regis Hanol
+# url: https://github.com/discourse/discourse-spoiler-alert
 
 register_asset "javascripts/spoiler.js"
