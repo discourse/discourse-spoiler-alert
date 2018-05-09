4270c6866eb3864ab0d0e98da3625f51d5b33f90
Régis Hanol <regis@hanol.fr>
2018-05-09 19:44:00 +0200

    FIX: remove spoilers in embedded comments

--- plugin.rb
+++ plugin.rb
@@ -7,18 +7,20 @@
 enabled_site_setting :spoiler_enabled
 
 register_asset "javascripts/spoiler.js"
-
 register_asset "stylesheets/discourse_spoiler_alert.css"
 
 after_initialize do
 
   # black out spoilers in emails
   Email::Styles.register_plugin_style do |fragment|
-    if SiteSetting.spoiler_enabled
-      fragment.css(".spoiler").each do |spoiler|
-        spoiler["style"] = "color:\#000;background-color:\#000;"
-      end
+    fragment.css(".spoiler").each do |spoiler|
+      spoiler["style"] = "color: #000; background-color: #000;"
     end
+  end
+
+  # remove spoilers in embedded comments
+  on(:reduce_cooked) do |fragment|
+    fragment.css(".spoiler").remove
   end
 
 end
