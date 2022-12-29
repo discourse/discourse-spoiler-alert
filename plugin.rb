4abf32e28cecf5486efc956c70fec9ab499a34f4
David Taylor <david@taylorhq.com>
2022-12-29 12:34:51 +0000

    DEV: Introduce syntax_tree for ruby formatting (#97)

--- plugin.rb
+++ plugin.rb
@@ -13,16 +13,16 @@ register_asset "stylesheets/discourse_spoiler_alert.scss"
 
 after_initialize do
   on(:reduce_cooked) do |fragment, post|
-    fragment.css(".spoiler").each do |el|
-      link = fragment.document.create_element("a")
-      link["href"] = post.url
-      link.content = I18n.t("spoiler_alert.excerpt_spoiler")
-      el.inner_html = link.to_html
-    end
+    fragment
+      .css(".spoiler")
+      .each do |el|
+        link = fragment.document.create_element("a")
+        link["href"] = post.url
+        link.content = I18n.t("spoiler_alert.excerpt_spoiler")
+        el.inner_html = link.to_html
+      end
   end
 
   # Remove spoilers from topic excerpts
-  on(:reduce_excerpt) do |doc, post|
-    doc.css(".spoiler").remove
-  end
+  on(:reduce_excerpt) { |doc, post| doc.css(".spoiler").remove }
 end
--- spec/pretty_text_spec.rb
+++ spec/pretty_text_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe PrettyText do
   let(:post) { Fabricate(:post) }
@@ -9,24 +9,25 @@ describe PrettyText do
     html.strip
   end
 
-  it 'can spoil blocks' do
+  it "can spoil blocks" do
     md = PrettyText.cook("[spoiler]\nmy tests fail\n[/spoiler]")
     html = "<div class=\"spoiler\">\n<p>my tests fail</p>\n</div>"
 
     expect(md).to eq(html)
   end
 
-  it 'can spoil inline' do
-    md = PrettyText.cook('I like watching [spoiler]my tests fail[/spoiler]')
+  it "can spoil inline" do
+    md = PrettyText.cook("I like watching [spoiler]my tests fail[/spoiler]")
     html = '<p>I like watching <span class="spoiler">my tests fail</span></p>'
 
     expect(md).to eq(html)
   end
 
-  it 'can replace spoilers in emails' do
-    md = PrettyText.cook('I like watching [spoiler]my tests fail[/spoiler]')
+  it "can replace spoilers in emails" do
+    md = PrettyText.cook("I like watching [spoiler]my tests fail[/spoiler]")
     md = PrettyText.format_for_email(md, post)
-    html = "<p>I like watching <span class=\"spoiler\"><a href=\"#{post.full_url}\">spoiler</a></span></p>"
+    html =
+      "<p>I like watching <span class=\"spoiler\"><a href=\"#{post.full_url}\">spoiler</a></span></p>"
 
     expect(md).to eq(html)
   end
